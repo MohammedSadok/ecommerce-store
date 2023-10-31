3a79637b7c07e47bcf01f6b5906c5611796be25b
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-10-31 11:32:39 +0100

    finish work until configure stripe for payment

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import ProductList from "@/components/ProductsList";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("2abddd47-6bf5-4f8f-84a1-3ce991acb983");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
